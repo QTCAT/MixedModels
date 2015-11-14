0c92583c75a2a12693ff8fa68962d56cfcb79fe6
Jonas <jrklasen@gmx.de>
2015-11-14 20:29:27 +0100

    Moin Moin

---
+++ R/relationshipmmPackage.R
@@ -0,0 +1,11 @@
+#' @name relationshipmm-package
+#' @aliases relationshipmm
+#' @docType package
+#'
+#' @title HFit relationship matrices based mixed-effects model
+#'
+#' @description Fit relationship matrices based mixed-effects model.
+#'
+#' @author Jonas Klasen
+#'
+NULL

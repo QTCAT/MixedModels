1be013bb47f344922b4c79775c88d39d15de0842
jrklasen <jrklasen@gmx.de>
2015-11-18 16:31:15 +0100

    new arguments to ``grm`` and improved error messages

--- R/mm.R
+++ R/mm.R
@@ -5,10 +5,14 @@
 #' @param x a matrix (n x p) of n individuals (rows) and p SNPs (columns). The
 #' alleles have to be coded by AA = 0, AB = 0.5, and BB = 1. Missing data are
 #' not yet allowed.
+#' @param checkPD logickal, if true (default) the nearest positive definite 
+#' matrix to the GRM is compute if the GRM does not fulfill these 
+#' requirements anyways.
+#' @param ... additional arguments to \code{\link[Matrix]{nearPD}}.
 #'
 #' @importFrom Matrix nearPD
 #' @export
-grm <- function(x) {
+grm <- function(x, checkPD = TRUE, ...) {
   stopifnot(is.matrix(x))
   stopifnot(!is.na(x))
   if (!all(na.omit(unique(as.vector(x))) %in% c(0, .5, 1)))
@@ -17,8 +21,14 @@ grm <- function(x) {
   v <- mean(f * (1 - f))
   w <- x - matrix(rep(f, each = nrow(x)), nrow(x))
   A <- tcrossprod(w) / (ncol(x) / 2 * v)
-  tryCatch(expr = nearPD(A)$mat,
-           error = message("it was not possible to find a positive definite matrix"))
+  if (checkPD)
+    A <- tryCatch(nearPD(A, ...)$mat,
+                  error = function(x) {
+                    message("it was not possible to find a positive definite matrix")
+                    message(x)
+                    return(NULL)
+                  })
+  A
 }
 
 
@@ -28,12 +38,13 @@ grm <- function(x) {
 #' defined 'G-site' covariance relationship matrices. Derifed from 
 #' \code{\link[pedigreemm]{pedigreemm}}.
 #' 
-#' @inheritParams lme4::lmer
+#' @inheritParams lme4::lmer 
 #' @param family a GLM family, see \code{\link[stats]{glm}} and 
 #' \code{\link[stats]{family}}.
 #' @param covarrel a named list of relationship matrices. The names must 
 #' correspond to the names of grouping factors for random-effects terms in the 
 #' \code{formula} argument.
+#' 
 #' 
 #' @details All arguments to this function are the same as those to the 
 #' function \code{\link[lme4]{lmer}} (or in case of family 
@@ -81,12 +92,11 @@ relMM <-  function(formula, data, family = NULL, REML = TRUE,
   # check the covarrel argument
   stopifnot(is.list(covarrel),
             length(names(covarrel)) == length(covarrel))
-  if (!all(sapply(covarrel,
-                  function(x) {
-                    nrow(x) == ncol(x) &&
-                      all(rownames(x) == colnames(x)) &&
-                      (inherits(x, "Matrix") || inherits(x, "matrix"))
-                  })))
+  if (!all(sapply(covarrel, function(x) {
+    nrow(x) == ncol(x) && 
+      all(rownames(x) == colnames(x)) && 
+      (inherits(x, "Matrix") || inherits(x, "matrix"))
+  })))
     stop("every relatinships matrix must be a square matrix which identical column and row names")
   lmf <- eval(lmerc, parent.frame())
   # copy the pedigree list for relfactor
@@ -95,33 +105,35 @@ relMM <-  function(formula, data, family = NULL, REML = TRUE,
   stopifnot(all(pnms %in% names(lmf@flist)))
   asgn <- attr(lmf@flist, "assign")
   Zt <- lmf@pp$Zt
-  for (i in seq_along(covarrel)) { # i <- 1
-    tn <- which(match(pnms[i], names(lmf@flist)) == asgn) # tn <- 1
+  for (i in seq_along(covarrel)) {
+    tn <- which(match(pnms[i], names(lmf@flist)) == asgn)
     if (length(tn) > 1)
-      stop("a relatinships matrix must be associated with only one r.e. term")
+      stop("each relationship matrix must be associated with only one random-effect term")
     ind <- (lmf@Gp)[tn:(tn + 1L)]
-    rowsi <- (ind[1] + 1L):ind[2]
-    stopifnot(setequal(rownames(covarrel[[i]]), rownames(Zt)[rowsi]))
-    relfac[[i]] <- tryCatch(chol(covarrel[[i]]),
+    Zti <- (ind[1] + 1L):ind[2]
+    if (!all(rownames(Zt)[Zti] %in% rownames(covarrel[[i]])))
+      stop("levels of a random-effect term are not part of the according relationship matrix")
+    relfaci <- match(rownames(covarrel[[i]]), rownames(Zt)[Zti])
+    relfac[[i]] <- tryCatch(chol(covarrel[[i]][relfaci, relfaci]),
                             error = function(x) {
-                              message("a relatinships matrix is not positive definite")
+                              message("each relatinships matrices must be positive semidefinite")
                               message(x)
                               return(NULL)
                             })
-    Zt[rowsi, ] <- relfac[[i]] %*% Zt[rowsi, ]
+    Zt[Zti, ] <- relfac[[i]] %*% Zt[Zti, ]
   }
   reTrms <- list(Zt = Zt, theta = lmf@theta, Lambdat = lmf@pp$Lambdat,
                  Lind = lmf@pp$Lind, lower = lmf@lower, flist = lmf@flist,
                  cnms = lmf@cnms, Gp = lmf@Gp)
   dfl <- list(fr = lmf@frame, X = lmf@pp$X, reTrms = reTrms, start = lmf@theta)
   if (gaus) {
-    dfl$REML = lmf@resp$REML > 0L
+    dfl$REML <- lmf@resp$REML > 0L
     devfun <- do.call(mkLmerDevfun, dfl)
-    opt <- optimizeLmer(devfun, optimizer = "Nelder_Mead",...)
+    opt <- optimizeLmer(devfun, optimizer = "Nelder_Mead", ...)
   } else {
     dfl$family <- family
     devfun <- do.call(mkGlmerDevfun, dfl)
-    opt <- optimizeGlmer(devfun, optimizer = "Nelder_Mead",...)
+    opt <- optimizeGlmer(devfun, optimizer = "Nelder_Mead", ...)
   }
   mm <- mkMerMod(environment(devfun), opt, reTrms, lmf@frame, mc)
   cls <- if (gaus) "lmerpedigreemm" else "glmerpedigreemm"
